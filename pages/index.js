89b4f15580146bfb58dd61d3621c15049c98866d
Hamza Waleed <hamzawaleed0102@gmail.com>
2019-06-24 17:08:44 +0500

    Route Masking

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Header from '../components/Header';
 import Layout from '../components/MyLayout';
 const PostLink = props => (
     <li>
-        <Link href={`/post?title=${props.title}`}>
+        <Link href={`/post?title=${props.title}`} as={`/p/${props.id}`}>
             <a>{props.title}</a>
         </Link>
     </li>
@@ -12,9 +12,9 @@ const Index = () => (
     <Layout>
         <h1>My Blog</h1>
         <ul>
-            <PostLink title="Hello Next.js" />
-            <PostLink title="Next.js is awesome" />
-            <PostLink title="Deploy Apps with Zeit" />
+            <PostLink title="Hello Next.js" id="hello-nextjs" />
+            <PostLink title="Next.js is awesome" id="learn-nextjs" />
+            <PostLink title="Deploy Apps with Zeit" id="deploy-nextjs" />
         </ul>
     </Layout>
 )
